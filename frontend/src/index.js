2edf0901c125ce0024bdcdaa47fa9dff752c7a8d
Giovanna Monti <ladyarcadia98@live.it>
2021-10-22 17:04:21 +0200

    removed comments

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,10 +4,3 @@ import { TodoListContainer } from "./components/TodoListContainer.js"
 import "./index.css"
 
 ReactDOM.render(<TodoListContainer />, document.getElementById("root"))
-
-/* todo 
-separa i componenti in moduli
-capisci come scrivere su file json
-prova a fare la delete/update passando parametri nell'url (REST)
-prova a usare patch invece che post per pare l'update (utilizzando sia il parametro rest per l'id che il body per le robe da aggiornare (credo): 'modifica questo elemento con i dati che ti passo io' )
-*/
